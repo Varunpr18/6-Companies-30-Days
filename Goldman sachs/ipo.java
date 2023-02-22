76ccc607f139946f35350db17d30e0f44e68c462
Varunpr18 <107846895+Varunpr18@users.noreply.github.com>
2023-02-22 09:50:28 +0530

    Update ipo.java

--- Goldman sachs/ipo.java
+++ Goldman sachs/ipo.java
@@ -1,3 +1,4 @@
+//6th-prblm
 public class Solution {
     public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
         PriorityQueue<int[]> pqCap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
