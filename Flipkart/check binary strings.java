c9cf31fee5cf7298e980652932370eee99d81fe8
Varunpr18 <107846895+Varunpr18@users.noreply.github.com>
2023-02-22 09:36:20 +0530

    Update check binary strings.java

--- Flipkart/check binary strings.java
+++ Flipkart/check binary strings.java
@@ -1,37 +1,4 @@
-// class Solution {
-//     public boolean hasAllCodes(String s, int k) {
-//           if(s.length() < k){
-//             return false;
-//         }
-
-//         HashSet<String> set = new HashSet<>();
-
-//         for(int i=0;i<=s.length() - k ;i++){
-
-//             set.add(s.substring(i, i+k));
-//         }return set.size() == Math.pow(2, k);
-
-//         // if(set.size() == Math.pow(2, k)){
-//         //     return true;
-//         // }
-//         // return false;
-//     }
-// }
-
-// class Solution{
-//     public boolean hasAllcodes(String s,int k){
-//         if(s.length()<k){
-//             return false;
-//         }
-
-//         HashSet<String> set = new HashSet<>();
-//         for(int i=0;i<=s.length-k;i++){
-//             set.add(s.substring(i,i+k));
-//         }return set.size() == Math.pow(2,k);
-      
-// }
-// }
-
+//4th-prblm
 class Solution {
     public boolean hasAllCodes(String s, int k) {
           if(s.length() < k){
